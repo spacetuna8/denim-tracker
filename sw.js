8b5692129c679c6b3cefbc499e2401a678e34939
spacetuna8 <noax.oxner@gmail.com>
2026-05-09 14:10:16 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const C = 'denim-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(['./index.html'])));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))
+  ));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
